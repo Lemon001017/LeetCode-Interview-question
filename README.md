b1c616f625387ea45b81defd832c2b3570fc82a3
liguangyu <1727857539@qq.com>
2024-08-17 15:42:10 +0800

    finish: RemoveDuplicatesTwo

--- README.md
+++ README.md
@@ -2,6 +2,7 @@
 
 1. Merge two sorted arrays  **Link**: https://leetcode.com/problems/merge-sorted-array/
 2. Remove element **Link**: https://leetcode.com/problems/remove-element/
-3. Remove duplicates from a sorted array **Link**: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+3. Remove duplicates from a sorted array | **Link**: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+4. Remove duplicates from a sorted array || **Link**: https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
 
 
---
+++ RemoveDuplicatesTwo.java
@@ -0,0 +1,26 @@
+public class RemoveDuplicatesTwo {
+    /*
+        题目: 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素最多出现两次，返回移除后数组的新长度。
+     */
+
+    public static int removeDuplicates(int[] nums) {
+        int k = 2;
+        for (int i = 2; i < nums.length; i++) {
+            if (nums[i] != nums[k - 2]) {
+                nums[k++] = nums[i];
+            }
+        }
+        return k;
+    }
+
+    public static void assertEqual(Object actual, Object expected, String message) {
+        if (!actual.equals(expected)) {
+            throw new AssertionError("actual: " + actual + ", expected: " + expected + " " + message);
+        }
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {1, 1, 1, 2, 2, 3};
+        assertEqual(removeDuplicates(nums), 5, "");
+    }
+}
