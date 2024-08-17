87a4f804be848d46e13768cd4b03787c06fbf1cc
liguangyu <1727857539@qq.com>
2024-08-17 15:55:14 +0800

    finish: MajorityElement

---
+++ MajorityElement.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class MajorityElement {
+    /*
+        题目: 找出数组中出现次数超过一半的元素
+     */
+
+    public static int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[(nums.length - 1) / 2];
+    }
+
+    public static void assertEqual(Object actual, Object expected, String message) {
+        if (!actual.equals(expected)) {
+            throw new AssertionError("actual: " + actual + ", expected: " + expected + " " + message);
+        }
+    }
+
+    public static void main(String[] args) {
+        assertEqual(majorityElement(new int[]{3, 2, 3}), 3, "");
+        assertEqual(majorityElement(new int[]{2, 2, 1, 1, 1, 2, 2}), 2, "");
+    }
+}
--- README.md
+++ README.md
@@ -4,5 +4,5 @@
 2. Remove element **Link**: https://leetcode.com/problems/remove-element/
 3. Remove duplicates from a sorted array | **Link**: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 4. Remove duplicates from a sorted array || **Link**: https://leetcode.com/problems/remove-duplicates-from-sorted-array-ii/
-
+5. Find the elements that appear more than half the time **Link**: https://leetcode.com/problems/majority-element/
 
