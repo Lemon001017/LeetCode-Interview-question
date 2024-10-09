90be6641e1daa884aab6c377113ab77c0cc0ca97
liguangyu <1727857539@qq.com>
2024-10-09 17:19:57 +0800

    finish IsValidBST

---
+++ BinaryTree/IsValidBST.java
@@ -0,0 +1,52 @@
+package BinaryTree;
+
+public class IsValidBST {
+    /**
+     * 题目: 判断是否为二叉搜索树
+     */
+    static class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public static boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public static boolean isValidBST(TreeNode node, long lower, long upper) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= lower || node.val >= upper) {
+            return false;
+        }
+        return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+    }
+
+    public static void assertEqual(Object actual, Object expected, String message) {
+        if (!actual.equals(expected)) {
+            throw new AssertionError("actual: " + actual + ", expected: " + expected + " " + message);
+        }
+    }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(2);
+        node1.left = new TreeNode(1);
+        node1.right = new TreeNode(3);
+        assertEqual(isValidBST(node1), true, "1");
+    }
+}
--- README.md
+++ README.md
@@ -99,6 +99,7 @@
 7. 二叉树的右视图 https://leetcode.com/problems/binary-tree-right-side-view/
 8. 二叉树中的最大路径和 https://leetcode.com/problems/binary-tree-maximum-path-sum/
 9. 将有序数组转换为二叉搜索树 https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/
+10. 验证二叉搜索树 https://leetcode.com/problems/validate-binary-search-tree/
 
 ## 图论
 
